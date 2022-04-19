a4db02d9e45ece7fa3d5b10d9c5e05c152d70f3f
Justin Swift <jlswift2@gmail.com>
2022-04-19 11:21:04 -0500

    Alter status message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     def show
         user = User.find_by(id: session[:user_id])
         if user
-            render json: user, status: :created
+            render json: user, status: :ok
         else
             render json: {error: "Not authorized"}, status: :unauthorized
         end
