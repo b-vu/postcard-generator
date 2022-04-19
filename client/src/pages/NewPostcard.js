9378a68a6cadb99402178913b42aa93ed8f65dd8
Brian Vu <brianvu7@gmail.com>
2022-04-19 02:39:08 -0500

    added canvas functionality to backend storage

--- client/src/pages/NewPostcard.js
+++ client/src/pages/NewPostcard.js
@@ -10,8 +10,10 @@ function NewPostcard() {
   function handleFormSubmit(e) {
     e.preventDefault();
 
+    //creating the FormData object to be easily sent in an HTTP request
     let formData = new FormData();
 
+    //appending the image key with the uploaded image in the FormData object
     formData.append("image", selectedFile);
 
     // POST request for uploaded images
@@ -21,19 +23,32 @@ function NewPostcard() {
     })
     .then(res => res.json())
     .then(data => console.log(data));
+  }
 
-    // Testing POST request for image URL's
-    // fetch("/postcards", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify({
-    //     image_url: imageUrl
-    //   })
-    // })
-    // .then(res => res.json())
-    // .then(data => console.log(data));
+  function submitImage(e) {
+    //converts the canvas to base-64 data that represents a file type(defaults to PNG)
+    const data = e.target.parentNode.children[0].toDataURL()
+
+    //creating the FormData object to be easily sent in an HTTP request
+    let formData = new FormData();
+
+    //appending the image key with the file data in the FormData object
+    formData.append("image", data)
+
+    fetch("/postcards", {
+      method: "POST",
+      body: formData
+    })
+    .then(res => res.json())
+    .then(data => console.log(data));
+  }
+
+  //function for adding text to the canvas
+  function addText(e) {
+    const canvas = e.target.parentNode.children[0];
+    const canvasContext = canvas.getContext("2d");
+    canvasContext.font = "30px Arial";
+    canvasContext.fillText("Hello World",10,50);
   }
 
   return (
@@ -43,7 +58,14 @@ function NewPostcard() {
         <input type="file" onChange={handleSelectedFileChange}></input>
         <button>Submit</button>
       </form>
-      {/* <img src='/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--067ea5ad84b1c04d315a5a444e92863574e47c83/pexels-helena-lopes-2253275.jpg' alt="Dog"></img> */}
+
+      <div>
+      <canvas width="300" height="300" style={{ border: "1px solid #d3d3d3" }}></canvas>
+        <button onClick={submitImage}>Submit</button>
+        <button onClick={addText}>Add Text</button>
+      </div>
+
+      {/* <img src='' alt="Test"></img> */}
     </div>
   )
 }
