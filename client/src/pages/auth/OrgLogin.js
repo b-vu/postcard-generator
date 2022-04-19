d2b044cb3999a542b463c2dd39098f7ec950196c
Justin Swift <jlswift2@gmail.com>
2022-04-19 09:29:34 -0500

    Build preliminary org login

---
+++ client/src/pages/auth/OrgLogin.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+
+function OrgLogin({ setUser }) {
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        password_confirmation: "",
+        email: "",
+        name: "",
+        manager_name: ""
+    });
+    const [isSigningUp, setIsSigningUp] = useState(false);
+    
+    let navigate = useNavigate();
+    
+    function handleFormChange(e) {
+    setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+    });
+    }
+
+    function onLogin(user) {
+    setUser(user)
+    navigate("/")
+    }
+
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        if (isSigningUp) {
+            fetch("/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            })
+            .then(res => res.json())
+            .then(user => onLogin(user));
+        } 
+        else {
+            fetch("/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            })
+            .then((r) => r.json())
+            .then(user => onLogin(user));
+        }
+    }
+    
+    function handleSignUpStateChange() {
+        setIsSigningUp(!isSigningUp);
+    }
+
+
+    return (
+        <div>
+            {
+            isSigningUp ?
+            <form onSubmit={handleFormSubmit}>
+                Sign Up
+                <input type="text" name="username" value={formData.username} onChange={handleFormChange} placeholder="Username"></input>
+                <input type="password" name="password" value={formData.password} onChange={handleFormChange} placeholder="Password"></input>
+                <input type="password" name="password_confirmation" value={formData.password_confirmation} onChange={handleFormChange} placeholder="Password Confirmation"></input>
+                <input type="text" name="email" value={formData.email} onChange={handleFormChange} placeholder="Email"></input>
+                <input type="text" name="institution_name" value={formData.name} onChange={handleFormChange} placeholder="Institution Name"></input>
+                <input type="text" name="name" value={formData.name} onChange={handleFormChange} placeholder="Manager's First Name"></input>
+                <button>Submit</button>
+
+                Already have an account? <button onClick={handleSignUpStateChange}>Login</button>
+            </form>
+            :
+            <form onSubmit={handleFormSubmit}>
+                Login
+                <input type="text" name="username" value={formData.username} onChange={handleFormChange} placeholder="Username"></input>
+                <input type="password" name="password" value={formData.password} onChange={handleFormChange} placeholder="Password"></input>
+                <button>Submit</button>
+
+                Don't have an account? <button onClick={handleSignUpStateChange}>Sign up</button>
+            </form>
+            }
+        </div>
+    )
+}
+
+
+export default OrgLogin
