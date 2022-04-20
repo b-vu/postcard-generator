1c2d93844a7a4af91514d5b30b991c1eb32064ca
Justin Swift <jlswift2@gmail.com>
2022-04-20 15:56:34 -0500

    create index and create endpoints for Institutions

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
   resources :postcards, only: [:index, :show, :create]
   resources :institutions, only: [:index]
-
+  resources :recipients, only: [:index]
+ 
   #standard user signup/login/logout
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
+
   post '/login', to: 'sessions#create'
   post '/inst-login', to: 'sessions#inst_create'
   delete '/inst-logout', to: 'sessions#inst_destroy'
   delete '/logout', to: 'sessions#destroy'
+
   get '/inst', to: 'institutions#show'
   post '/inst-signup', to: 'institutions#create'
+
+  post '/new_recipient', to: 'recipients#create'
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
