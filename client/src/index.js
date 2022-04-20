a841bf47b1f3392123e783ea95336ef3ce045dd9
Brian Vu <brianvu7@gmail.com>
2022-04-19 19:13:54 -0500

    I give up

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <Router>
-    <Routes>
-      <Route path="/" element={ <App></App> }>
-      </Route>
-    </Routes>
+    <App />
   </Router>,
   document.getElementById('root')
 );
