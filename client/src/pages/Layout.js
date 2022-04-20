1e583c3b523e0dab79334157deea23a56c708ab8
Justin Swift <jlswift2@gmail.com>
2022-04-20 10:41:27 -0500

    Created child, Navbar, and moved logic from Layout

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -1,44 +1,13 @@
 import React from 'react'
-import { Outlet, useNavigate } from "react-router-dom"
+import { Outlet } from "react-router-dom"
+import Navbar from "./Layout/Navbar"
 
 function Layout({ user, setUser }) {
-  let navigate = useNavigate()
-
-  function handleLogout () {
-    if (user.manager_name) {
-      fetch("/inst-logout", {
-        method: "DELETE",
-      }).then(() => onLogout());
-    } 
-    else {
-      fetch("/logout", {
-        method: "DELETE",
-      }).then(() => onLogout());
-    }
-  }
-
-  function onLogout() {
-    setUser(null)
-    navigate('/')
-  }
-
-  function handleLogin() {
-    navigate("/select")
-  }
-
-  let button
-  if (user){
-    button = <button onClick={handleLogout}>Logout</button>
-  } else {
-    button = <button onClick={handleLogin}>Login</button>
-  }
-
-
   return (
     <div>      
       <h1>Layout</h1>
       <main>
-        {button}
+        <Navbar user={user} setUser={setUser} />
         <Outlet />
       </main>
     </div>
