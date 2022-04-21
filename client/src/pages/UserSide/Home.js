78f0098c8d68055ee0ffc49765bb4028e862860b
Jane Kim <jane.kim0717@gmail.com>
2022-04-21 03:28:18 -0400

    Home page/login/new postcard styling

--- client/src/pages/UserSide/Home.js
+++ client/src/pages/UserSide/Home.js
@@ -4,20 +4,54 @@ function Home({ user }) {
   console.log(user)
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <>
+      <header className="bg-header container flex items-center justify-center h-screen mx-auto">
+        <div className="font-serif mx-4 p-4 text-center md:p-8">
+          <h1 className="text-8xl uppercase font-bold">
+            POSTCARD GENERATOR
+          </h1>
+        </div>
+      </header>
+
+      <div className="font-serif leading-normal mx-auto py-12 px-4 max-w-lg">
+        <p className="mb-6 text-xl md:text-4xl uppercase font-bold">WHO WE ARE.</p>
+        <p className="mb-4 text-lg">Quae commodi reiciendis dignissimos. Rerum molestiae hic dolores assumenda dolor! Corrupti reiciendis maxime fuga, recusandae obcaecati officia cum temporibus dicta quibusdam praesentium, magni, cumque aperiam iusto sequi illum molestiae non.</p>
+        <p className="mb-4 text-lg">In excepturi repellendus eum, qui corrupti rerum perferendis harum adipisci voluptate? Nihil, quidem deleniti libero officia dicta vel asperiores velit molestiae blanditiis, dolore voluptatibus excepturi laudantium at veniam illo. Dolor!</p>
+      </div>
+
+      <div className="bg-quote container flex items-center justify-center h-screen mx-auto">
+        <blockquote className="bg-black font-serif mx-4 p-4 text-center text-white md:p-8">
+          <p className="font-bold italic text-3xl">
+            &ldquo;Providing friendships, one postcard at a time.&rdquo;
+          </p>
+        </blockquote>
+      </div>
+
+      <div className="font-serif leading-normal mx-auto py-12 px-4 max-w-lg">
+        <p className="mb-6 text-xl md:text-4xl uppercase font-bold">HOW TO GET STARTED.</p>
+        <p className="mb-4 text-lg">Quae commodi reiciendis dignissimos. Rerum molestiae hic dolores assumenda dolor! Corrupti reiciendis maxime fuga, recusandae obcaecati officia cum temporibus dicta quibusdam praesentium, magni, cumque aperiam iusto sequi illum molestiae non.</p>
+        <p className="mb-4 text-lg">In excepturi repellendus eum, qui corrupti rerum perferendis harum adipisci voluptate? Nihil, quidem deleniti libero officia dicta vel asperiores velit molestiae blanditiis, dolore voluptatibus excepturi laudantium at veniam illo. Dolor!</p>
+      </div>
+
+      <div className="bg-footer container flex items-center justify-center h-screen mx-auto">
+      </div>
+
+      <div className="font-serif leading-normal mx-auto py-12 px-4 max-w-lg">
+        <p className="mb-6 text-xl md:text-4xl uppercase font-bold">ORGANIZATION ADMINSTRATORS.</p>
+        <p className="mb-4 text-lg">Quae commodi reiciendis dignissimos. Rerum molestiae hic dolores assumenda dolor! Corrupti reiciendis maxime fuga, recusandae obcaecati officia cum temporibus dicta quibusdam praesentium, magni, cumque aperiam iusto sequi illum molestiae non.</p>
+        <p className="mb-4 text-lg">In excepturi repellendus eum, qui corrupti rerum perferendis harum adipisci voluptate? Nihil, quidem deleniti libero officia dicta vel asperiores velit molestiae blanditiis, dolore voluptatibus excepturi laudantium at veniam illo. Dolor!</p>
+      </div>
+
       {/* <div className='text-5xl'>Home</div> */}
-      {
+      {/* {
         user ? 
         user.postcards.map(postcard => {
           return <img src={postcard.image.url} alt="Postcard"></img>
         })
         :
         null
-      }
-    </div>
+      } */}
+    </>
   )
 }
 
--- client/src/pages/auth/Select.js
+++ client/src/pages/auth/Select.js
@@ -10,9 +10,17 @@ function Select() {
 
   return (
     <div>
-        <h2>Are you a single user or and organization?</h2>
-        <button value="user" onClick={handleRedirect}>user</button>
-        <button value="organization" onClick={handleRedirect}>organization</button>
+        <div className='bg-white h-screen flex flex-col justify-center items-center'>
+          <h2 className='lg:text-5xl md:text-4xl sm:text-3xl text-2xl font-black mb-14 font-serif'>
+            Are you a single user or an organization?
+          </h2>
+
+          <button className='font-serif text-center py-6 px-10 bg-stone-200 rounded-full text-3xl hover:bg-stone-400 transition duration-300 ease-in-out flex items-center animate-bounce"' value="user" onClick={handleRedirect}>User</button>
+          <br/>
+          <button className='font-serif text-center py-6 px-10 bg-stone-200 rounded-full text-3xl hover:bg-stone-400 transition duration-300 ease-in-out flex items-center animate-bounce"' value="organization" onClick={handleRedirect}>Organization</button>
+
+        </div>
+        
     </div>
   )
 }
