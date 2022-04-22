49b21b37766220ce61cfe297e586d4afed9b04d4
Jane Kim <jane.kim0717@gmail.com>
2022-04-22 10:38:27 -0400

    nav bar

--- client/src/pages/Layout/Navbar.js
+++ client/src/pages/Layout/Navbar.js
@@ -60,10 +60,22 @@ function Navbar( {user, setUser} ) {
 
 
     return (
-    <nav>
-        {homeLink}
-        {newCreation}
-        {button}
+    <nav className='flex justify-between items-center py-8 mx-auto h-20 bg-stone-200 font-sans text-black text-xl sticky top-0 z-30 w-full px-2 py-4 sm:px-4 shadow-sm' role='navigation'>
+        <div className="flex items-center">
+            <svg class="h-8 w-8 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />  <polyline points="22,6 12,13 2,6" /></svg>
+            <h3 class="px-2 text-2xl font-medium text-black">POSTCARD GENERATOR</h3>
+        </div>
+        <div className='pr-10 md:block'>
+            <button className='p-4 bg-stone-200 rounded-full hover:bg-stone-400 transition duration-300 ease-in-out hover:animate-bounce'>
+                {homeLink}
+            </button>
+            <button className='p-4 bg-stone-200 rounded-full hover:bg-stone-400 transition duration-300 ease-in-out hover:animate-bounce'>
+                {newCreation}
+            </button>
+            <button className='p-4 bg-stone-200 rounded-full hover:bg-stone-400 transition duration-300 ease-in-out hover:animate-bounce'>
+                {button}
+            </button>
+        </div>
     </nav>
     )
 }
--- client/src/pages/UserSide/Home.js
+++ client/src/pages/UserSide/Home.js
@@ -15,8 +15,8 @@ function Home({ user }) {
 
       <div className="font-serif leading-normal mx-auto py-12 px-4 max-w-lg">
         <p className="mb-6 text-xl md:text-4xl uppercase font-bold">WHO WE ARE</p>
-        <p className="mb-4 text-lg">Postcard Generator is a free service dedicated to providing individuals in hospitals or assisted-living facilities with uplifting cards from caring individuals across the globe. </p>
-        <p className="mb-4 text-lg">In difficult times, a small postcard can have a big impact. </p>
+        <p className="mb-4 text-xl">Postcard Generator is a free service dedicated to providing individuals in hospitals or assisted-living facilities with uplifting cards from caring individuals across the globe. </p>
+        <p className="mb-4 text-xl">In difficult times, a small postcard can have a big impact. </p>
       </div>
 
       <div className="bg-quote container flex items-center justify-center h-screen mx-auto">
@@ -28,19 +28,19 @@ function Home({ user }) {
       </div>
 
       <div className="font-serif leading-normal mx-auto py-12 px-4 max-w-lg">
-        <p className="mb-6 text-xl md:text-4xl uppercase font-bold">HOW TO GET STARTED.</p>
-        <p className="mb-4 text-lg">Providing friendship through a postcard is simple. After signing up, you can use our in-browser editor to create something that is unique that only you can make. Upload images, draw pictures, write messages; the possibilities to make an inspiring postcard are endless.</p>
-        <p className="mb-4 text-lg">After you submit your card, it will be send to the manager of the organization you chose, and they will hand-deliver it!</p>
+        <p className="mb-6 text-xl md:text-4xl uppercase font-bold">HOW TO GET STARTED</p>
+        <p className="mb-4 text-xl">Providing friendship through a postcard is simple. After signing up, you can use our in-browser editor to create something that is unique that only you can make. Upload images, draw pictures, write messages; the possibilities to make an inspiring postcard are endless.</p>
+        <p className="mb-4 text-xl">After you submit your card, it will be sent to the manager of the organization you choose, and they will hand-deliver it!</p>
       </div>
 
       <div className="bg-footer container flex items-center justify-center h-screen mx-auto">
       </div>
 
       <div className="font-serif leading-normal mx-auto py-12 px-4 max-w-lg">
-        <p className="mb-6 text-xl md:text-4xl uppercase font-bold">ORGANIZATION ADMINSTRATORS.</p>
-        <p className="mb-4 text-lg">If you are an organization wishing to get postcards in the hands of your patients, you can easily add recipients to our database through the user-friendly interface. Changes to your list are painless so that you can spend less time on our site and more time with those who depend on you.</p>
-        <p className="mb-4 text-lg">In addition to adding recipients, you will also be able to view, save, and print postcards sent to your patients in a few clicks.</p>
-        <p className="mb-4 text-lg">Have questions? Email us at admin@pg.com</p>
+        <p className="mb-6 text-xl md:text-4xl uppercase font-bold">ORGANIZATION ADMINSTRATORS</p>
+        <p className="mb-4 text-xl">If you are an organization wishing to get postcards in the hands of your patients, you can easily add recipients to our database through our user-friendly interface. Changes to your list are painless so that you can spend less time on our site and more time with those who depend on you.</p>
+        <p className="mb-4 text-xl">In addition to adding recipients, you will also be able to view, save, and print postcards sent to your patients in a few clicks.</p>
+        <p className="mb-4 text-xl">Have questions? Email us at admin@pg.com</p>
       </div>
 
       {/* <div className='text-5xl'>Home</div> */}
